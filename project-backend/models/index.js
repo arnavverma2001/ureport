400600ee64cf01d0773868ac42e98876d824c998
Thomas Kamm <33606565+Nopekick@users.noreply.github.com>
2018-09-13 20:35:20 -0700

    Update index.js

--- project-backend/models/index.js
+++ project-backend/models/index.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 
 mongoose.set('debug', true);
 mongoose.Promise = Promise
-mongoose.connect("mongodb://hackuser:reactwin2@ds251022.mlab.com:51022/srchacks")
+mongoose.connect("mongodb://usern:pswrd@ds251022.mlab.com:51022/srchacks")
 
 module.exports.User = require("./user")
